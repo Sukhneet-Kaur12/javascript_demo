4af29f9f7f6c03ce42cae424a807105f0e38bad8
Sukhneet-Kaur12 <118029664+Sukhneet-Kaur12@users.noreply.github.com>
2024-05-03 11:45:03 -0500

    while loop

--- Part 3/sandbox_part3.js
+++ Part 3/sandbox_part3.js
@@ -2,6 +2,7 @@
 
 // inside the brackets, there is a condition if it follows along then only the the code will go to the curly bracket command. 
 // and it will execute and loop until the value of i becomes lass than 5.
+// i = 0 is initialization, i < 5 is condition, i++ is final expression
 for(let i = 0; i < 5; i++){
     console.log('in loop:', i);
 }
@@ -26,6 +27,20 @@ for(let i = 0; i < names.length; i++){ // we used length to count the number of 
 for(let i = 0; i < names.length; i++){ // we used length to count the number of names in the provide data.
     let html = `<div>${names[i]}</div>`;
     console.log(html);
+}
+
+// While loop
+// only difference between for and while loop is syntax. 
+//let i = 0; // in while loop, we declare i outisde the loop
+// while(i < 5){
+//     console.log('in loop:', i);
+//     i++;
+// }
+
+let i = 0;
+while(i < names.length){
+    console.log(names[i]);
+    i++;
 }
 
 
