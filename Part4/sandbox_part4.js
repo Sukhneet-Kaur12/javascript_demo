c65ade8dfceffbfb201e4d9fbbf4583bb0c1092d
Sukhneet-Kaur12 <118029664+Sukhneet-Kaur12@users.noreply.github.com>
2024-05-04 22:51:00 -0500

    function declaration

---
+++ Part4/sandbox_part4.js
@@ -0,0 +1,43 @@
+//Function showScores()
+//Function convertCoords, it converts the coordinates into addesses. for eg: convertCoords(1029,869)
+
+// function declaration
+function greet(){
+    console.log('hello there');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
