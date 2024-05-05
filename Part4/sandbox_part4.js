44244315aa7876b130d74607429fbd19edf5ef96
Sukhneet-Kaur12 <118029664+Sukhneet-Kaur12@users.noreply.github.com>
2024-05-04 22:59:07 -0500

    function expression

--- Part4/sandbox_part4.js
+++ Part4/sandbox_part4.js
@@ -1,17 +1,20 @@
 //Function showScores()
 //Function convertCoords, it converts the coordinates into addesses. for eg: convertCoords(1029,869)
 
-// function declaration
+// function declaration, in ihis we donot store function inside variable
 function greet(){
     console.log('hello there');
 }
 
-// function expression
+// function expression, here we store function inside varible
+const speak = function(){
+    console.log('good day');
+}
 
 greet();
 greet(); // we could do it multiple times.
-greet();
 
+speak();
 
 
 
