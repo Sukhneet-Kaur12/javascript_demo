41a7e9acdb82aa4714fb324df837cbf1199a20be
Sukhneet-Kaur12 <118029664+Sukhneet-Kaur12@users.noreply.github.com>
2024-05-09 09:01:48 -0500

    arrow functions

--- Part4/sandbox_part4.js
+++ Part4/sandbox_part4.js
@@ -37,13 +37,22 @@ function greet(){
 const Speak = function(name = 'luigi', time = 'night'){
     console.log(`Good ${time} ${name}`);
 };
-Speak();
-Speak('Mario')
-Speak('mario', 'morning');
+Speak(); // the output will take the defined or declared varibales or values for name and time. eg. good night luigi
+Speak('Mario') // here, the output will take the name of mario but hte time is not defined do it will be same as declared in the function. eg, good night mario. 
+Speak('mario', 'morning'); //here the name and time is defined so it will be like good morning mario.
 
+// Regular functions
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// }
 
+// arrow function
+const calcArea = (radius) => {
+    return 3.14 * radius**2;
+}
 
-
+const area = calcArea(5);
+console.log('area is:', area);
 
 
 
