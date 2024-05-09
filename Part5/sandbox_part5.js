d7e1f6eff5349328437debeabd059aadd21db1ca
Sukhneet-Kaur12 <118029664+Sukhneet-Kaur12@users.noreply.github.com>
2024-05-09 15:45:15 -0500

    adding methods to the object

--- Part5/sandbox_part5.js
+++ Part5/sandbox_part5.js
@@ -2,24 +2,56 @@
 // Objects in javascript are those which can ahve properties and things they can do(method).
 // for example, blog object can have properties (title, content, author) and methods(publish, unpublish, delete)
 
+// let user = {
+//     name: 'crystal',
+//     age: 30,
+//     email: 'crystal@thenetninja.co.uk',
+//     location: 'berlin',
+//     blogs: ['why mac & cheese rules', '10 things to make with marmite']
+// };
+
+// console.log(user);
+// console.log(user.name);
+
+// // user.age = 35;
+// console.log(user.age);
+
+// console.log(user['email']); // to display the email
+// console.log(user['location']); // to display the location.
+
+// user['name'] = 'chun-li'; // now the name will be chun-li instead of crystal.
+// console.log(user.name);
+
+// console.log(typeof user);
+
+
+// how to add methods to this object
+
 let user = {
     name: 'crystal',
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    }
 };
 
-console.log(user);
-console.log(user.name);
+user.login();
+user.logout();
 
-// user.age = 35;
-console.log(user.age);
 
-console.log(user['email']); // to display the email
-console.log(user['location']); // to display the location.
 
-user['name'] = 'chun-li'; // now the name will be chun-li instead of crystal.
-console.log(user.name);
 
-console.log(typeof user);
+
+
+
+
+
+
+
+
